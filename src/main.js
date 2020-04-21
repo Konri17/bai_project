45f4cc8275489f03dfca6883d3644687de523dbb
Konri17 <konrad.flaka@gmail.com>
2020-04-21 22:35:45 +0200

    Vuex added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import 'es6-promise/auto'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
